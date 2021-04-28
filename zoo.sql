c4407f8c0efabe2d0cb319539b051da4e4603787
Tanzila Abedin <tanzila.abedin@yahoo.com>
2021-04-28 11:03:22 +0200

    add select basics answer

--- zoo.sql
+++ zoo.sql
@@ -1,1 +1,12 @@
+-- select from basics
+
+SELECT population FROM world
+  WHERE name = 'Germany';
+  
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway' , 'Denmark');
+  
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 and 250000;
+  
 
