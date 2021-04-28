1e4c7ca4ecfa26db756b2ad053e75576d730c4e2
Tanzila Abedin <tanzila.abedin@yahoo.com>
2021-04-28 20:28:01 +0200

    add select from nobel answers

--- zoo.sql
+++ zoo.sql
@@ -54,13 +54,72 @@ SELECT name, capital
  FROM world
 WHERE LENGTH(name) = LENGTH(capital) 
 
-
 SELECT name, capital 
  FROM world
 WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
 
-
 SELECT name
-   FROM world
+ FROM world
 WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%'
 AND name NOT LIKE '% %' AND name LIKE '%o%' AND name LIKE '%u%'
+
+-- SELECT from Nobel Tutorial
+
+SELECT yr, subject, winner
+  FROM nobel
+WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+WHERE yr = 1962
+  AND subject = 'Literature'
+  
+SELECT yr, subject 
+ FROM nobel 
+WHERE winner = 'Albert Einstein' 
+
+SELECT winner
+ FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+
+SELECT yr,subject, winner 
+ FROM nobel
+WHERE yr BETWEEN 1980 AND 1989 AND subject ='Literature'
+
+SELECT winner
+ FROM nobel
+WHERE winner LIKE 'John%'
+
+SELECT * 
+ FROM nobel
+WHERE (yr = 1980 AND Subject ='Physics' OR yr = 1984 AND subject = 'Chemistry')
+
+SELECT * 
+ FROM nobel
+WHERE yr = 1980 AND Subject NOT IN ('Medicine','Chemistry')
+
+SELECT *
+ FROM nobel
+WHERE yr < 1910 AND subject = 'Medicine' OR yr >= 2004 AND subject = 'Literature'
+
+SELECT * FROM nobel
+ WHERE winner LIKE 'PETER GRÜNBERG%'
+
+SELECT * FROM nobel
+ WHERE winner LIKE 'EUGENE O_NEILL'
+ 
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'sir%' 
+ORDER BY yr DESC
+
+SELECT winner,subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+ 
+ 
+ 
+ 
+ 
+ 
+ 
