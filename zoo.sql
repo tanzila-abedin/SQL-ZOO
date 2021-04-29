339fc5f95f415aa21b2ae9c1396b8714189561c7
Tanzila Abedin <tanzila.abedin@yahoo.com>
2021-04-29 16:34:11 +0200

    add SUM and COUNT answers

--- zoo.sql
+++ zoo.sql
@@ -170,3 +170,62 @@ SELECT continent, name FROM world x
   WHERE name <= ALL
     (SELECT name FROM world y
      WHERE y.continent=x.continent AND area>0) 
+                    
+-- SUM and COUNT
+                    
+SELECT SUM(population)
+FROM world
+
+SELECT DISTINCT(continent)
+FROM world
+   
+SELECT SUM(gdp)
+ FROM world
+WHERE continent = 'Africa'
+                    
+SELECT count(name) 
+ FROM world
+WHERE area >= 1000000
+                    
+SELECT SUM(population)
+ FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+                    
+SELECT continent,COUNT(name)
+ FROM world
+GROUP BY continent
+                    
+SELECT continent, COUNT(name)
+ FROM world
+WHERE population >= 10000000
+GROUP BY continent
+                    
+SELECT continent
+ FROM world
+GROUP BY continent
+HAVING sum(population) >= 100000000
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
