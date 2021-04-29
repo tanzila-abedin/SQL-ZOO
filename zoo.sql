4bc087b5a7995c33079086efa888e7ec8e367173
Tanzila Abedin <tanzila.abedin@yahoo.com>
2021-04-29 22:17:58 +0200

    add Using Null answers

--- zoo.sql
+++ zoo.sql
@@ -327,6 +327,63 @@ WHERE movieid IN(
 SELECT movieid FROM casting
 JOIN actor ON actor.id = casting.actorid
 WHERE name = 'Art Garfunkel')) AND name <> 'Art Garfunkel' ORDER BY name
+
+-- Using Null
+
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+
+
+SELECT name, 
+CASE 
+  WHEN mobile IS NULL THEN '07986 444 2266' 
+  ELSE mobile 
+  END
+FROM teacher
+
+SELECT name, COALESCE(mobile,'07986 444 2266')
+FROM teacher
+
+SELECT teacher.name, COALESCE(dept.name,'None')
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT COUNT(name),COUNT(mobile)
+FROM teacher
+
+SELECT dept.name ,COUNT(dept)
+From teacher RIGHT JOIN dept ON dept.id = teacher.dept
+GROUP BY dept.name
+
+SELECT name,
+CASE dept
+WHEN 1 THEN 'Sci'
+WHEN 2 THEN 'Sci'
+ELSE 'Art'
+END
+FROM teacher
+
+SELECT name,
+CASE dept
+WHEN 1 THEN 'Sci'
+WHEN 2 THEN 'Sci'
+WHEN 3 THEN 'Art'
+ELSE 'None'
+END
+FROM teacher
                     
                     
                     
