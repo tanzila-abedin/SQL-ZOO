082ad834915751546028c7b6701d41eb6196b226
Tanzila Abedin <tanzila.abedin@yahoo.com>
2021-04-29 21:27:51 +0200

    add The JOIN operation answers

--- zoo.sql
+++ zoo.sql
@@ -205,6 +205,44 @@ SELECT continent
 GROUP BY continent
 HAVING sum(population) >= 100000000
                     
+-- The JOIN operation
+                    
+SELECT matchid, player 
+ FROM goal 
+ WHERE  teamid = 'GER'
+                    
+SELECT DISTINCT id,stadium,team1,team2
+  FROM game
+WHERE id= 1012
+
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid ='GER'
+
+SELECT player, teamid,coach, gtime
+  FROM goal JOIN eteam on (teamid=id) 
+ WHERE gtime<=10
+
+SELECT mdate,teamname
+ FROM game JOIN eteam ON (team1 = eteam.id)
+WHERE coach = 'Fernando Santos'
+
+SELECT player
+FROM game 
+JOIN goal ON (game.id = goal.matchid)
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT matchid,mdate, COUNT(mdate)
+  FROM game JOIN goal ON matchid = id 
+ WHERE teamid = 'GER' GROUP BY matchid,mdate
+
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game JOIN goal ON matchid = id GROUP BY mdate,matchid,team1,team2
+ORDER BY mdate,matchid,team1,team2
                     
                     
                     
